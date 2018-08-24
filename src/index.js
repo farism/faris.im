0d6cf80560c9027dddbad8b7e50fd3593399ad20
Faris Mustafa <farismmk@gmail.com>
2018-08-24 02:59:35 -0700

    initial app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('app'))
+}
+
+render()
+
+if (module.hot) {
+  module.hot.accept(render)
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader?modules=true', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
